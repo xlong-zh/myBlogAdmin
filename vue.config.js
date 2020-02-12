816f4cbe673ac519b825205ffa85eb75ce075968
xlong-zh <907737948@qq.com>
2020-02-12 23:57:53 +0800

    2-12

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
   // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
   productionSourceMap: false,
   //基本路径
-  publicPath: './',
+  publicPath: '/admin/',
   // 输出文件目录
   outputDir: process.env.VUE_APP_CURRENTMODE === 'devtest' ? 'devtest' : 'dist',
 
